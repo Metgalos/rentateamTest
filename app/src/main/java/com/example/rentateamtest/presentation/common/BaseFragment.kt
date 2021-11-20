f532cb93582c8f17010d44e7961394b7060935b7
Volkov Alexandr <fckhimki1997@gmail.com>
2021-11-20 23:58:22 +0300

    Шаблон для навигации и базовый фрагмент

---
+++ app/src/main/java/com/example/rentateamtest/presentation/common/BaseFragment.kt
@@ -0,0 +1,26 @@
+package com.example.rentateamtest.presentation.common
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<V : ViewBinding>(
+    private val inflate: (LayoutInflater, ViewGroup?, Boolean) -> V,
+) : Fragment() {
+
+    private var _binding: V? = null
+    private val binding: V
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = inflate(inflater, container, false)
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/rentateamtest/presentation/main/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.rentateamtest.presentation.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.rentateamtest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding
+        get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
