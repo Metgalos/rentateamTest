008a67513363e8c71b7a691e91da2325d802a417
Volkov Alexandr <fckhimki1997@gmail.com>
2021-11-21 00:48:12 +0300

    Создание классов для работы с сетью в DI

---
+++ app/src/main/java/com/example/rentateamtest/common/di/NetworkModule.kt
@@ -0,0 +1,44 @@
+package com.example.rentateamtest.common.di
+
+import com.example.rentateamtest.data.network.UsersApiService
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(
+                HttpLoggingInterceptor()
+                    .setLevel(HttpLoggingInterceptor.Level.BODY)
+            )
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofitBuilder(client: OkHttpClient): Retrofit.Builder {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+            .client(client)
+    }
+
+    @Provides
+    @Singleton
+    fun provideUsersApiService(builder: Retrofit.Builder): UsersApiService {
+        return builder
+            .baseUrl(UsersApiService.BASE_URL)
+            .build()
+            .create(UsersApiService::class.java)
+    }
+}
